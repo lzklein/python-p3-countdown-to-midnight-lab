4ce5e69fb3f2044d4569ca003e0b98376e8e9581
Louis Klein <louisklein71@gmail.com>
2023-06-14 10:04:59 -0700

    Test

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,16 @@
 # your code goes here!
+import time
+def countdown(seconds):
+    i=seconds
+    while i > 0:
+        print(f'{i} SECOND(S)!')
+        i -=1
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(seconds):
+    i=seconds
+    while i > 0:
+        print(f'{i} SECOND(S)!')
+        i-=1
+        time.sleep(1)
+    print("HAPPY NEW YEAR!")
